238030b00273db9b7e2a0d9b28d815880686addd
Andrey Arzamaskov <arzamaskov@gmail.com>
2026-08-18 14:27:04 +0400

    feat: add minimal Wails application

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	err := wails.Run(&options.App{
+		Title:  "Runtracker",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+	})
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
